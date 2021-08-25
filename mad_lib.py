9f2fde951af9f6b0c5deeccffb8dab7928a2e49b
jpierce424 <71786124+jpierce424@users.noreply.github.com>
2021-08-25 10:10:13 -0400

    Add files via upload

---
+++ mad_lib.py
@@ -0,0 +1,7 @@
+Verb= input("Enter a Verb: ")
+plural_noun = input("Enter a plural noun ")                                                    
+color = input("Enter a food: ")
+
+print("I" + Verb "ran")
+print ("a mile " + plural_noun)
+print ("I am going to run a mile")
